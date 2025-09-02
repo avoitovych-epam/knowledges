fa0a66add32ce7aea80e6235d8f3fef1cbe8c14f
avoitovych_epam <andrii_voitovych@epam.com>
2025-09-02 11:57:46 +0200

    raise_next_app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+'use client';
+
+import { useEffect } from "react";
 import Image from "next/image";
 
 export default function Home() {
-  return (
+
+  useEffect(() => {
+    // Call the API route to create the table
+    fetch('/api/init-table');
+  }, []);
+
+    return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
